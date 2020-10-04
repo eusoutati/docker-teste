9712590d4bfeb7a2aee26eae152677ca8df62432
Tatiane <tatysantos.dl@gmail.com>
2020-10-03 21:33:21 -0300

    add alteration

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM openjdk
+
+FROM openjdk:8-jre-alpine
 
 WORKDIR /app
-
 COPY target/api-0.0.1-SNAPSHOT.jar /app/api.jar
 
 ENTRYPOINT ["java" , "-jar" , "api.jar"]
--- src/main/java/com/spring/api/controller/PingController.java
+++ src/main/java/com/spring/api/controller/PingController.java
@@ -1,14 +1,16 @@
 package com.spring.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ping")
 public class PingController {
-	
-	public void ping(){
-		System.out.println("Tudo funcionando");
+
+	@GetMapping
+	public String ping(){
+		return "Tudo funcionando";
 	}
 	
 	
